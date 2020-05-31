29e1439466172664dc408a7e8591daa28b2568df
fatmaalsmadi <fatmaalsmadi77@gmail.com>
2020-05-31 13:08:46 +0300

    register

---
+++ main.js
@@ -0,0 +1,104 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Validation(){
+ 
+ 
+  var form= document.getElementsByClassName(' form');
+  var email= document.getElementById('email').value;
+  var txt= document.getElementById('txt');
+var pattern = /^[^  ]+@{^ ]+\.[a-z]{2,3}$/;
+
+if(email.match(pattern))
+{
+
+form.classList.add("valid");
+
+form.classList.remove("invalid");
+txt.innerHTML = "your email address in valid.";
+txt.style.color ="#00ff00"
+}
+else{
+  form.classList.remove("valid");
+  form.classList.add("invalid");
+  txt.innerHTML = "please enter valid email address.";
+  txt.style.color ="#ff0000"
+}
+}
+
+var myInput = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {  
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
+  
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {  
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {  
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+  
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
